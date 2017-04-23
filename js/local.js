dc1dbf934538a226fd84f9daec1d35e6f2b59fb9
Hollyw00d <matt@mattjennings.net>
2017-04-22 21:03:55 -0700

    middle of figuring out hash issues

--- js/local.js
+++ js/local.js
@@ -4,46 +4,88 @@
 
   // Start jQuery Document Ready
   $(document).ready(function() {
-
-    // Portfolio page (front page) code to show and hide project categories AND
-    // code runs ONLY if on Portfolio page (front page)
-    if( $("body.home").length > 0 ) {
+    function portofolioChooser() {
 
       // Variables for the hash change and SELECT tag JS
+      var showOverrideClass = "show-override";
+      var hideOverrideClass = "hide-override";
+      var hashSelectedClass = "hash-selected";
       var featuredProjectsDataAttr = "featured-projects";
       var featuredProjectsSection = $("#" + featuredProjectsDataAttr + "-section");
       var allProjectsSection = $("#all-projects-section");
       var portfolioProjectChooser = $("#portfolio-project-chooser");
 
-      portfolioProjectChooser.on("change", function() {
-        var chosenOptionTagDataAttr = $(this).find("option:selected").attr("data-project-category");
+      // Show featured items only
+      function showFeatured(featuredId) {
+        $(featuredId).removeClass(hideOverrideClass).addClass(showOverrideClass);
+      }
 
+      function hashChange(getSelectTag) {
+        var hash = location.hash.slice(1);
+        getSelectTag.find("option[data-project-category=" + hash + "]").prop("selected", "selected").addClass(hashSelectedClass);
 
-        // If OPTION tag chosen is NOT "featured-projects" then
-        // ONLY display portfolio projects from chosen portfolio projects, like "JavaScript and jQuery"
-        if( chosenOptionTagDataAttr !== featuredProjectsDataAttr ) {
-          if( featuredProjectsSection.hasClass("show-override") ) {
-            featuredProjectsSection.removeClass("show-override").addClass("hide-override");
-          }
-
+        if( hash === featuredProjectsDataAttr ) {
           allProjectsSection.find("h2").removeClass("show-override").addClass("hide-override");
           allProjectsSection.find("li").removeClass("show-inlineblock-override").addClass("hide-override");
 
-          allProjectsSection.find("h2[data-project-category=" + chosenOptionTagDataAttr + "]").removeClass("hide-override").addClass("show-override");
-          allProjectsSection.find("li[data-project-category=" + chosenOptionTagDataAttr + "]").removeClass("hide-override").addClass("show-inlineblock-override");
+          allProjectsSection.find("h2[data-project-category=" + hash + "]").removeClass("hide-override").addClass("show-override");
+          allProjectsSection.find("li[data-project-category=" + hash + "]").removeClass("hide-override").addClass("show-inlineblock-override");
         }
-        // ELSE "featured-projects" OPTION tag IS CHOSEN  then
-        // ONLY display "Featured Projects" portfolio item
         else {
-          if( featuredProjectsSection.hasClass("hide-override") ) {
-            featuredProjectsSection.removeClass("hide-override").addClass("show-override");
-          }
-
           allProjectsSection.find("h2").removeClass("show-override").addClass("hide-override");
           allProjectsSection.find("li").removeClass("show-inlineblock-override").addClass("hide-override");
         }
-      });
+      }
+
+      // Portfolio page (front page) code to show and hide project categories AND
+      // code runs ONLY if on Portfolio page (front page)
+      if( $("body.home").length > 0 ) {
+
+        showFeatured("#featured-projects-section");
+
+        portfolioProjectChooser.on("change", function() {
+          var chosenOptionTagDataAttr = $(this).find("option:selected").attr("data-project-category");
+          portfolioProjectChooser.find("option").removeClass(hashSelectedClass);
+
+          // If OPTION tag chosen is NOT "featured-projects" then
+          // ONLY display portfolio projects from chosen portfolio projects, like "JavaScript and jQuery"
+          if( chosenOptionTagDataAttr !== featuredProjectsDataAttr ) {
+
+            if( featuredProjectsSection.hasClass("show-override") ) {
+              featuredProjectsSection.removeClass("show-override").addClass("hide-override");
+            }
+
+            allProjectsSection.find("h2").removeClass("show-override").addClass("hide-override");
+            allProjectsSection.find("li").removeClass("show-inlineblock-override").addClass("hide-override");
+
+            allProjectsSection.find("h2[data-project-category=" + chosenOptionTagDataAttr + "]").removeClass("hide-override").addClass("show-override");
+            allProjectsSection.find("li[data-project-category=" + chosenOptionTagDataAttr + "]").removeClass("hide-override").addClass("show-inlineblock-override");
+            location.hash = "#" + chosenOptionTagDataAttr;
+          }
+          // ELSE "featured-projects" OPTION tag IS CHOSEN  then
+          // ONLY display "Featured Projects" portfolio item
+          else {
+            if( featuredProjectsSection.hasClass("hide-override") ) {
+              featuredProjectsSection.removeClass("hide-override").addClass("show-override");
+            }
+
+            allProjectsSection.find("h2").removeClass("show-override").addClass("hide-override");
+            allProjectsSection.find("li").removeClass("show-inlineblock-override").addClass("hide-override");
+            location.hash = "#" + featuredProjectsDataAttr;
+          }
+        });
+
+        var getHashChange = hashChange(portfolioProjectChooser);
+
+        $(window).on("hashchange", getHashChange);
+
+        //window.addEventListener("hashchange", hashChange, false);
+
+      }
+
+
     }
+    portofolioChooser();
 
   });
 
